85792df3eac3f1962a0226cd7b1072ba0029589e
Kelvin Dre <k3lvindre@gmail.com>
2021-05-30 22:55:48 +0800

    Fixed Delete not working, Added View and Controller for Account Api.

---
+++ HomeBudgetManagementMVC.Core/Models/AccountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeBudgetManagement.MVC.Core.Models
+{
+    public class AccountViewModel
+    {
+        public int Id { get; set; }
+        public double Balance { get; set; }
+        public double BalanceToAdd { get; set; }
+    }
+}
