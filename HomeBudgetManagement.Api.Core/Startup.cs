3d8fd35b59ecc2c4953eb37e5acc688450380d08
Kelvin Dre <k3lvindre@gmail.com>
2021-06-20 21:58:31 +0800

    Changed lifetime service of Repositories in Api.Core project.

--- HomeBudgetManagement.Api.Core/Startup.cs
+++ HomeBudgetManagement.Api.Core/Startup.cs
@@ -37,9 +37,9 @@ namespace HomeBudgetManagement.Api.Core
             //Singleton - which creates a single instance throughout the application.It creates the instance for the first time and reuses the same object in the all calls.
             //Scoped lifetime services - are created once per request within the scope.It is equivalent to a singleton in the current scope.For example, in MVC it creates one instance for each HTTP request, but it uses the same instance in the other calls within the same web request.
             //Transient lifetime services - are created each time they are requested.This lifetime works best for lightweight, stateless services.
-            services.AddSingleton<IExpenseRepository, ExpenseRepository>();
-            services.AddSingleton<IAccountRepository, AccountRepository>();
-            services.AddSingleton<IIncomeRepository, IncomeRepository>(); 
+            services.AddScoped<IExpenseRepository, ExpenseRepository>();
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<IIncomeRepository, IncomeRepository>(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
