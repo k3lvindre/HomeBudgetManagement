db5ab752814d9623f2e1a553019fa40513df14a5
Kelvin Dre <k3lvindre@gmail.com>
2021-11-03 18:31:19 +0800

    Removed expense and income models and there ef entities and use only one model "Item".

--- HomeBudgetManagement.Models/Item.cs
+++ HomeBudgetManagement.Models/Item.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HomeBudgetManagement.Models
 {
-    public class Income
+    //[Table("Item")] //commented out so all edm should only be configure inside dbcontext so it is easy to maintain and for separation of concern
+    public class Item
     {
         public int Id { get; set; }
-        public double Amount { get; set; }
         public string Description { get; set; }
+        public double Amount { get; set; }
         public DateTime Date { get; set; }
         public byte[] File { get; set; }
         public string FileExtension { get; set; }
-
+        public int ItemTypeId { get; set; }
     }
 }
