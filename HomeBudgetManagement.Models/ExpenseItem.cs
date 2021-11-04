1264d8542325ce02159e6746854a4a9559116ee1
Kelvin Dre <k3lvindre@gmail.com>
2021-11-04 11:00:34 +0800

    Added new model expenseitem

---
+++ HomeBudgetManagement.Models/ExpenseItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBudgetManagement.Models
+{
+    public class ExpenseItem
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+        public byte[] File { get; set; }
+        public string FileExtension { get; set; }
+        public int ExpenseItemTypeId { get; set; }
+    }
+}
