f2a778c1f90fb3d314fa62e5448be3be6ded3bc6
Kelvin Dre <k3lvindre@gmail.com>
2021-11-04 11:10:12 +0800

    Removed dbset for income and expense and instead added ExpenseItem so to use only single entity for both.

--- HomeBudgetManagement.Api.Core/Data/HomeBudgetManagementDbContext.cs
+++ HomeBudgetManagement.Api.Core/Data/HomeBudgetManagementDbContext.cs
@@ -13,18 +13,17 @@ namespace HomeBudgetManagement.Api.Core.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Expense>().ToTable("Expense");
-            modelBuilder.Entity<Expense>().HasKey(x => x.Id);
-            modelBuilder.Entity<Income>().ToTable("Income");
-            modelBuilder.Entity<Income>().HasKey(x => x.Id);
+            //modelBuilder.Entity<Expense>().ToTable("Expense");
+            //modelBuilder.Entity<Expense>().HasKey(x => x.Id);
+            //modelBuilder.Entity<Income>().ToTable("Income");
+            //modelBuilder.Entity<Income>().HasKey(x => x.Id);
             //Here we don't need to map account totable since the property name has the same name with the database's table account
             //modelBuilder.Entity<Account>().ToTable("Account");
-            modelBuilder.Entity<Account>().HasKey(x => x.Id);
+            //modelBuilder.Entity<Account>().HasKey(x => x.Id);
         }
 
 
-        public virtual DbSet<Expense> Expenses { get; set; }
-        public virtual DbSet<Income> Incomes { get; set; }
+        public virtual DbSet<ExpenseItem> ExpenseItems { get; set; }
         public virtual DbSet<Account> Account { get; set; }
 
     }
