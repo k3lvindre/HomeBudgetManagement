bc84fe41b83f49723b4b104ca4efb05affda4880
k3lvinDre <k3lvinDre@KELVINDRE-LAPTO>
2023-05-20 22:40:36 +0800

    Added type column to expense.

--- HomeBudgetManagement.Models/Expense.cs
+++ HomeBudgetManagement.Models/Expense.cs
@@ -6,11 +6,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HomeBudgetManagement.Models
 {
-    //[Table("Expense")] //commented out so all edm should only be configure inside dbcontext so it is easy to maintain and for separation of concern
-    public partial class Expense
+    //[Table("Expense")] //commented out so all edm should only be configure
+    //inside dbcontext so it is easy to maintain and for separation of concern
+    public class Expense
     {
         public int Id { get; set; }
         public string Description { get; set; }
+        public string Type { get; set; }
         public double Amount { get; set; }
         public DateTime Date { get; set; }
         public byte[] File { get; set; }
