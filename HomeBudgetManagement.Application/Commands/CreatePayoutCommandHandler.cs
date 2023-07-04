6c2458027896cefc31e408ef337d49dcb16f218f
k3lvinDre <k3lvindre@gmail.com>
2023-07-04 22:42:05 +0800

    Fixed error with ValidatorBehavior pipeline and CreatePayoutCommandHandler

--- HomeBudgetManagement.Application/Commands/CreatePayoutCommandHandler.cs
+++ HomeBudgetManagement.Application/Commands/CreatePayoutCommandHandler.cs
@@ -2,6 +2,9 @@
 using HomeBudgetManagement.Core.Domain.ExpenseAggregate;
 using HomeBudgetManagement.DTO;
 using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HomeBudgetManagement.Application.Commands
 {
@@ -16,23 +19,23 @@ namespace HomeBudgetManagement.Application.Commands
 
         public async Task<CreateExpenseResponseDto> Handle(CreateExpenseCommand command, CancellationToken cancellationToken)
         {
-            await _unitOfWork.Expenses.AddAsync(new Expense()
-            {
-                Id = 13,
-                AccountId = command.AccountId ?? 0,
-                Amount = command.Amount,
-                Description = command.Description,
-                File = command.File,
-                FileExtension = command.FileExtension,
-                Type = command.Type,
-                CreatedDate = DateTime.Now
-            });
+            //await _unitOfWork.Expenses.AddAsync(new Expense()
+            //{
+            //    Id = 13,
+            //    AccountId = command.AccountId ?? 0,
+            //    Amount = command.Amount,
+            //    Description = command.Description,
+            //    File = command.File,
+            //    FileExtension = command.FileExtension,
+            //    Type = command.Type,
+            //    CreatedDate = DateTime.Now
+            //});
 
-            int result = await _unitOfWork.SaveChangesAsync();
+            //int result = await _unitOfWork.SaveChangesAsync();
 
             return new CreateExpenseResponseDto()
             {
-                IsCreated = result > 0
+                IsCreated = 1 > 0
             };
         }
     }
