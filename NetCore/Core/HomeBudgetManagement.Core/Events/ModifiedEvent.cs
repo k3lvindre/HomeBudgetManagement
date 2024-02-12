673c8762c7b537a995fbe3cab4e4dffe42b47115
k3lvinDre <k3lvinDre@gmail.com>
2024-02-12 23:16:52 +0800

    Change ExpenseModifiedEvent to generic type constraint ModifiedEvent

---
+++ NetCore/Core/HomeBudgetManagement.Core/Events/ModifiedEvent.cs
@@ -0,0 +1,15 @@
+﻿using HomeBudgetManagement.Core.Domain.ExpenseAggregate;
+using MediatR;
+
+namespace HomeBudgetManagement.Core.Events
+{
+    public class ModifiedEvent<T> : INotification
+    {
+        public T Entity { get; }
+
+        public ModifiedEvent(T entity)
+        {
+            Entity = entity;
+        }
+    }
+}
