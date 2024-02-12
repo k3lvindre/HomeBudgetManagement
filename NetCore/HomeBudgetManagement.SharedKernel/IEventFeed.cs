84975347779428edad967f4724f7fbba2e06bce6
k3lvinDre <k3lvinDre@gmail.com>
2024-02-12 23:02:57 +0800

    Add SharedKernel project.

---
+++ NetCore/HomeBudgetManagement.SharedKernel/IEventFeed.cs
@@ -0,0 +1,9 @@
+﻿
+namespace HomeBudgetManagement.SharedKernel
+{
+    public interface IEventFeed
+    {
+        Task PublishAsync(string eventName, object content);
+        Task<List<object>> GetByNameAsync(string eventName);
+    }
+}
---
+++ NetCore/HomeBudgetManagement.SharedKernel/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace HomeBudgetManagement.SharedKernel
+{
+    //Unlike in HomeBudgetManagement.API project here we removed the implementation of IDisposable because .net core handles the disposal by adding scoped or transient lifetime in startup
+    //Here, we are creating the IGenericRepository interface as a Generic Interface
+    //Here, we are applying the Generic Constraint 
+    //The constraint is T which is going to be a class
+    public interface IGenericRepository<T> where T : class
+    {
+        Task AddAsync(T entity);
+
+        Task AddRangeAsync(IList<T> entities);
+
+        void Delete(T entity);
+
+        List<T> ExecuteQuery(string sql);
+
+        List<T> ExecuteQuery(string sql, object[] sqlParametersObjects);
+
+        List<T> GetAll();
+
+        Task<List<T>> GetAllAsync();
+
+        Task<T?> GetByIdAsync(int id);
+
+        void Update(T entity);
+    }
+}
