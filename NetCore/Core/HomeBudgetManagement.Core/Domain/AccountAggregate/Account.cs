663428d0c8a15131d1bb46135400b6095c830ddd
k3lvinDre <k3lvinDre@gmail.com>
2024-02-12 23:14:05 +0800

    Delete account class from core project

--- NetCore/Core/HomeBudgetManagement.Core/Domain/AccountAggregate/Account.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HomeBudgetManagement.Core.Domain.AccountAggregate
-{
-    public class Account : BaseEntity
-    {
-        public required string AccountName { get; set; }
-        public required double Balance { get; set; }
-    }
-}
