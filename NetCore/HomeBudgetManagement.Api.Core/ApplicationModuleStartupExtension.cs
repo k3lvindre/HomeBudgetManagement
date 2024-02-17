65b28b11d60ffada50a5c317ebac59db94768e95
k3lvinDre <k3lvinDre@gmail.com>
2024-02-17 17:33:53 +0800

    ExpenseRepository to BudgetRepository at api project.

--- NetCore/HomeBudgetManagement.Api.Core/ApplicationModuleStartupExtension.cs
+++ NetCore/HomeBudgetManagement.Api.Core/ApplicationModuleStartupExtension.cs
@@ -1,4 +1,4 @@
-﻿using HomeBudgetManagement.Core.Domain.ExpenseAggregate;
+﻿using HomeBudgetManagement.Core.Domain.BudgetAggregate;
 using HomeBudgetManagement.Infrastructure.EntityFramework;
 using HomeBudgetManagement.Infrastructure.EntityFramework.Repositories;
 using HomeBudgetManagement.Infrastructure.EventFeed;
@@ -15,7 +15,7 @@ namespace HomeBudgetManagement.Api.Core
         {
             services.AddDbContext<HomeBudgetManagementDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("HbmConnectionString")), ServiceLifetime.Scoped);
             //must replace with func<string, IGenericRepository> to get IGenericRepository by name in case of diff. implementation of IGenericRepository
-            services.AddTransient<IExpenseRepository, ExpenseRepository>();
+            services.AddTransient<IBudgetRepository, BudgetRepository>();
             services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             return services;
