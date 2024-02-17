aa1fecf1c2c06eee271dbcd3b28df7089a9b48be
k3lvinDre <k3lvinDre@gmail.com>
2024-02-17 15:35:49 +0800

    Added spa support for Front end.

--- FrontEnd/HomeBudgetManagement.Spa.React/Program.cs
+++ FrontEnd/HomeBudgetManagement.Spa.React/Program.cs
@@ -1,17 +1,27 @@
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+    configuration.RootPath = "ClientApp";
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
 
+app.UseSpaStaticFiles();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+});
 
 app.Run();
