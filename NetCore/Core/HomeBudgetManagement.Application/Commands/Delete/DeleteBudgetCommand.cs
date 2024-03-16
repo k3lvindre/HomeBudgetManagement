c87ac9dad568bbef76e1eb47c97e0732da511cf0
k3lvinDre <k3lvinDre@gmail.com>
2024-03-16 13:18:28 +0800

    added delete command

---
+++ NetCore/Core/HomeBudgetManagement.Application/Commands/Delete/DeleteBudgetCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace HomeBudgetManagement.Application.Commands
+{
+    public class DeleteBudgetCommand: IRequest<bool>
+    {
+        public required int Id { get; set; }
+    }
+}
