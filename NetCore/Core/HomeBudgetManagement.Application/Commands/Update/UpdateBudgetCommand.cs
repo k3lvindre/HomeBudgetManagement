931fc8eac08c91cd8ab2e8e4e66ec9916af30ae7
k3lvinDre <k3lvinDre@gmail.com>
2024-03-16 13:18:37 +0800

    add update command

---
+++ NetCore/Core/HomeBudgetManagement.Application/Commands/Update/UpdateBudgetCommand.cs
@@ -0,0 +1,15 @@
+﻿using HomeBudgetManagement.SharedKernel.ValueObjects;
+using MediatR;
+
+namespace HomeBudgetManagement.Application.Commands
+{
+    public class UpdateBudgetCommand: IRequest<bool>
+    {
+        public required int Id { get; set; }
+        public required string Description { get; set; }
+        public required double Amount { get; set; }
+        public ItemType ItemType { get; set; }
+        public byte[]? File { get; set; }
+        public string? FileExtension { get; set; }
+    }
+}
---
+++ NetCore/Core/HomeBudgetManagement.Application/Commands/Update/UpdateBudgetCommandValidator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+
+namespace HomeBudgetManagement.Application.Commands
+{
+    public class UpdateBudgetCommandValidator : AbstractValidator<UpdateBudgetCommand>
+    {
+        public UpdateBudgetCommandValidator()
+        {
+            RuleFor(obj => obj.Id).NotNull().GreaterThan(0)
+                .WithMessage("Id is required");            
+            
+            RuleFor(obj => obj.Description).NotEmpty()
+                .WithMessage("Description is required");
+
+            RuleFor(obj => obj.ItemType).NotNull()
+                .WithMessage("Type is required");
+
+            //Other examples...
+            //RuleFor(obj => obj.RequestID).Length(1, 50)
+            //    .WithMessage(ResponseCode.InvalidRequestId.IdString)
+            //    .NotEmpty()
+            //    .WithMessage(ResponseCode.InvalidRequestId.IdString)
+            //    .Must((obj, prop) => Guid.TryParse(obj.RequestID, out Guid guid) && guid != Guid.Empty)
+            //    .WithMessage(ResponseCode.InvalidRequestId.IdString);
+
+
+            //RuleFor(obj => obj.NotificationUrl)
+            //    .Must(obj => Uri.TryCreate(obj, UriKind.Absolute, out var _))
+            //    .Must(obj => obj.Length > 1 && obj.Length <= 255)
+            //    .When(obj => !string.IsNullOrEmpty(obj.NotificationUrl))
+            //    .WithMessage(ResponseCode.InvalidNotificationUrl.IdString);
+
+            //RuleFor(obj => obj.BeneficiaryEmail)
+            //     .Matches(@"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$")
+            //     .When(obj => !string.IsNullOrWhiteSpace(obj.BeneficiaryEmail))
+            //     .WithMessage(ResponseCode.InvalidBeneficiaryEmailAddress.IdString);
+        }
+    }
+}
