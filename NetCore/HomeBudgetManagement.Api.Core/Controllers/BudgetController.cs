4aa9779e88fc2123ac3280d9e9f15cb9e34f99e2
k3lvinDre <k3lvinDre@gmail.com>
2024-04-08 11:57:21 +0800

    test github actions

--- NetCore/HomeBudgetManagement.Api.Core/Controllers/BudgetController.cs
+++ NetCore/HomeBudgetManagement.Api.Core/Controllers/BudgetController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
-//test github actionsss
+//test github actionssss
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 namespace HomeBudgetManagement.Api.Core.Controllers
