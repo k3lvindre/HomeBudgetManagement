b25fa41c69c2c06531ca349f7b653d1b80fc71da
K3lvindre <K3lvindre@gmail.com>
2024-05-06 07:00:03 +0000

    root commit

--- NetCore/Core/HomeBudgetManagement.Core/Domain/BudgetAggregate/IFileAttachmentRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using HomeBudgetManagement.SharedKernel;
-
-namespace HomeBudgetManagement.Core.Domain.BudgetAggregate
-{
-    public interface IFileAttachmentRepository : IGenericRepository<FileAttachment>
-    {
-        Task<FileAttachment> GetByBudgetIdAsync(int Id);
-    }
-}
--- NetCore/HomeBudgetManagement.Api.Core/Services/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace HomeBudgetManagement.Api.Core.Services
-{
-    public interface IAuthService
-    {
-        public string GenerateToken(IdentityUser userAccount);
-    }
-}
